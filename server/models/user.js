024ab041420674e8546b21f37e1d2fe51e9184fd
Ann Cascarano <a.cascarano@gmail.com>
2017-06-18 14:45:04 -0400

    adding beginning user authentication

--- server/models/user.js
+++ server/models/user.js
@@ -1,12 +1,65 @@
-var mongoose = require('mongoose');
+const mongoose = require('mongoose');
+const validator = require('validator');
+const _ = require('lodash');
+const jwt = require('jsonwebtoken'); // JSON web token
 
-var User = mongoose.model('Users', {
+var UserSchema = new mongoose.Schema({
     email: {
         type: String,
         required: true,
         minlength: 1,
-        trim: true //removes any leading or trailing white spaces
-    }
+        trim: true, //removes any leading or trailing white spaces
+        unique: true,
+        validate: {
+            validator: (value) => {
+                return validator.isEmail(value);
+            },
+            message: '{VALUE} is not a valid email'
+        }
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 6
+    },
+    tokens: [{
+        access: {
+            type: String,
+            required: true
+        },
+        token: {
+            type: String,
+            required: true
+        }
+    }]
 });
+
+UserSchema.methods.toJSON = function() {
+    var user = this;
+    var userObject = user.toObject();
+
+    // make sure that we are only returning id & email - not the token and password
+    return _.pick(userObject, ['_id', 'email']);
+};
+
+
+// Using 'regular' function because need to use 'this' which doesn't work with arrow functions (does not bind)
+UserSchema.methods.generateAuthToken = function() {
+    var user = this;
+    var access = 'auth';
+    var token = jwt.sign({
+        _id: user._id.toHexString(),
+        access: access
+    }, 'abc123').toString();
+
+    user.tokens.push({
+        access, token
+    });
+    return user.save().then(() => {
+        return token;
+    });
+};
+
+var User = mongoose.model('Users', UserSchema);
 
 module.exports = { User };
--- server/server.js
+++ server/server.js
@@ -117,6 +117,24 @@ app.patch('/todos/:id', (req, res) => {
         })
 });
 
+
+// USERS
+
+// POST /users
+app.post('/users', (req, res) => {
+    var body = _.pick(req.body, ['email', 'password']);
+    var user = new User(body);
+
+    user.save().then(() => {
+            return user.generateAuthToken();
+        }).then((token) => {
+            res.header('x-auth', token).send(user);
+        }).catch((err) => {
+            console.log("Unable to create user"), 
+            res.status(400).send(err);
+        });
+});
+
 app.listen(port, () => {
     console.log(`Started on port ${port} \n`);
 });
