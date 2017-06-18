7236ad68e3e0409d1a0e5b56e9def3767bcc8c14
Ann Cascarano <a.cascarano@gmail.com>
2017-06-18 11:32:22 -0400

    add tests for patch/todos/id request

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -12,7 +12,9 @@ const todos = [{
     text: 'First test todo'
     }, {
      _id: new ObjectID(),
-    text: 'Second test todo' 
+    text: 'Second test todo',
+    completed: true,
+    completedAt: 333
 }];
 
 // Empty the list of ToDos - before each test!
@@ -138,5 +140,51 @@ describe('DELETE/todos/:id', () => {
 
 });
 
+describe('PATCH /todos/:id', () => {
+    it('should update the todo', (done) => {
+        // grab id of first item
+        var id = todos[0]._id.toHexString();
+        var newText = "Here is my changed text";
+        request(app)
+            .patch(`/todos/${id}`)
+            .send({ 
+                text: newText,
+                completed: true
+            })
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todo.text).toBe(newText);
+                expect(res.body.todo.completed).toBe(true);
+                expect(res.body.todo.completedAt).toBeA('number');
+            })
+            .end((err,res) => {
+                if(err) {
+                    return done(err);
+                } done();
+            });
+        });
+        it('should clear completedAt when todo is not completed', (done) => {
+            var id = todos[1]._id.toHexString();
+            var newText = "Here is my changed text, yo";
+            request(app)
+                .patch(`/todos/${id}`)
+                .send({ 
+                    text: newText,
+                    completed: false
+                })
+                .expect(200)
+                .expect((res) => {
+                    expect(res.body.todo.text).toBe(newText);
+                    expect(res.body.todo.completed).toBe(false);
+                    expect(res.body.todo.completedAt).toNotExist();
+                })
+                .end((err,res) => {
+                    if(err) {
+                        return done(err);
+                    } done();
+                });
+        });
+
+});
 
 // npm test
